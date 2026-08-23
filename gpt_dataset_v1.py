8c15d77f9c4f273c08d7f0a83f98f949908a2847
LYK <dalinaum@gmail.com>
2026-08-23 17:39:53 +0900

    Fix DataLoader worker argument typo

--- gpt_dataset_v1.py
+++ gpt_dataset_v1.py
@@ -23,7 +23,7 @@ class GPTDatasetV1(Dataset):
     
 def create_dataloader_v1(txt, batch_size=4, max_length=256,
                          stride=128, shuffle=True, drop_last=True,
-                         num_wordkers=0):
+                         num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
     dataloader = DataLoader(
@@ -31,7 +31,7 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
         batch_size=batch_size,
         shuffle=shuffle,
         drop_last=drop_last,
-        num_workers=num_wordkers
+        num_workers=num_workers
     )
 
     return dataloader
